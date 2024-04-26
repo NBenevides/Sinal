72d75869a6f498c207ef8d4dc04deff301209d72
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 19:34:18 -0300

    exercicio completo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+
+Console.Write("Digite um numero: ");
+
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else if (numero > 0)
+{
+        Console.WriteLine("Positivo");
+}
+else 
+{
+    Console.WriteLine("Zero");
+}
